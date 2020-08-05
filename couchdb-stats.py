ede47ae8670db8d03bd070766153855f16644506
Serj <msergg@users.noreply.github.com>
2020-08-05 14:04:50 +0300

    Update couchdb-stats.py

--- couchdb-stats.py
+++ couchdb-stats.py
@@ -1,6 +1,8 @@
+#!/bin/python
 import sys
 import json
 import urllib2
+import  base64
 
 def getFromDict(dataDict, mapList):
     for k in mapList:
@@ -15,7 +17,14 @@ if len(sys.argv[1]) < 1:
     print ' ex: ./couchdb-stats.py couchdb.httpd.aborted_requests.value'
 
 try:
-    req = urllib2.Request('http://127.0.0.1:5986/_stats')
+    couch_node='couchdb@127.0.0.1'
+    req = urllib2.Request('http://127.0.0.1:5984/_node/{}/_stats'.format(couch_node))
+    username = 'user'
+    password = 'password'
+
+    base64string = base64.b64encode('%s:%s' % (username, password))
+    req.add_header("Authorization", "Basic %s" % base64string)
+
     stats_json = urllib2.urlopen(req)
 except urllib2.URLError, e:
     print e.reason
